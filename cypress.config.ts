5cef7268d976a7fd3d8cefe37a39ce6d69626fd0
Corentin Recanzone <corentindevpro@hotmail.com>
2025-02-13 16:12:26 +0100

    m

--- cypress.config.ts
+++ cypress.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
         (spec: Cypress.Spec, results: CypressCommandLine.RunResult) => {
           if (results && results.video) {
             // Do we have failures for any retry attempts?
-            const failures = results.tests.some((test) => test.attempts.some((attempt) => attempt.state === 'failed'))
+            const failures: boolean = results.tests.some((test: CypressCommandLine.TestResult): boolean => test.attempts.some((attempt: CypressCommandLine.AttemptResult) => attempt.state === 'failed'))
             if (!failures) {
               // delete the video if the spec passed and no tests retried
               fs.unlinkSync(results.video)
