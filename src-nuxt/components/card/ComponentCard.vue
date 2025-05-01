b3796cd574263c5f0426217f5c76ee1cd6cf9d36
greglmx0 <gregoire.lamoureux@epitech.eu>
2025-05-01 17:51:05 +0200

    feat: add ComponentCard and HeroSection components; create AdminSidebar template

---
+++ src-nuxt/components/card/ComponentCard.vue
@@ -0,0 +1,57 @@
+<template>
+  <div @click="emitSelect()" class="card">
+    <h1>{{ props.label }}</h1>
+    <!-- <p>Type: {{ props.type }}</p> -->
+    <p v-if="props.icon">Icon: {{ props.icon }}</p>
+
+    <FlapiIcon
+      v-if="props.icon"
+      color="#fff"
+      :height="24"
+      mode="stroke"
+      :name="props.icon"
+      viewBox="0 0 24 24"
+      :width="24"
+    />
+  </div>
+</template>
+
+<script lang="ts" setup>
+/**
+ * @description
+ * This is the model value for the select field.
+ * @property {string} label - The label of the component.
+ * @property {string} type - The type of the component.
+ * @property {string} [icon] - The icon of the component.
+ */
+type FlapiComponentCardProps = {
+  label: string
+  type: string
+  icon?: string
+}
+
+const props: FlapiComponentCardProps = defineProps({
+  label: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    required: true,
+  },
+  icon: {
+    type: String,
+    default: '',
+  },
+})
+
+const emit: (event: 'select', ...args: any[]) => void = defineEmits(['select'])
+
+/**
+ * @description
+ * This function is used to emit the click event.
+ */
+const emitSelect: () => void = () => {
+  emit('select', props.type)
+}
+</script>
---
+++ src-nuxt/components/sections/HeroSection.vue
@@ -0,0 +1,58 @@
+<template>
+  <section>
+    <div>
+      <div>
+        <h1>{{ props.title }}</h1>
+        <p>{{ props.content }}</p>
+        <a v-if="props.showButton" :href="props.buttonLink">{{ props.buttonLabel }}</a>
+      </div>
+      <img v-if="props.showImage" :src="props.imageUrl" alt="Hero Image" />
+    </div>
+  </section>
+</template>
+
+<script lang="ts" setup>
+/**
+ * @description
+ */
+type FlapiHeroSectionProps = {
+  title: string
+  content: string
+  imageUrl: string
+  showImage: boolean
+  showButton: boolean
+  buttonLabel: string
+  buttonLink: string
+}
+
+const props: FlapiHeroSectionProps = defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  imageUrl: {
+    type: String,
+    required: true,
+  },
+  showImage: {
+    type: Boolean,
+    default: true,
+  },
+  showButton: {
+    type: Boolean,
+    default: true,
+  },
+  buttonLabel: {
+    type: String,
+    default: 'Learn More',
+  },
+  buttonLink: {
+    type: String,
+    default: '#',
+  },
+})
+</script>
